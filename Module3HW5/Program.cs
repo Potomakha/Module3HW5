b93bc01edd8279932ee3f93744ba3d56fb00e729
Oleh Potomakha <potomakha@gmail.com>
2021-12-20 20:48:28 +0200

    ReadFileAsync method ResultAsync method synchronized call in Main

--- Module3HW5/Program.cs
+++ Module3HW5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -6,9 +7,39 @@ namespace Module3HW5
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var resultTaks = await ResultAsunc();
+            Console.WriteLine(resultTaks);
+        }
+
+        private static async Task<string> ReadFileAsync(string path)
+        {
+            using (var reader = new StreamReader(path))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+
+        private static async Task<string> ResultAsunc()
+        {
+            return await Task<string>.Run(() =>
+            {
+                var taskList = new List<Task<string>>();
+
+                taskList.Add(ReadFileAsync(".\\Resources\\Hello.txt"));
+                taskList.Add(ReadFileAsync(".\\Resources\\World.txt"));
+
+                var result = string.Empty;
+
+                Task.WaitAll(taskList.ToArray());
+                foreach (var item in taskList)
+                {
+                    result += $"{item.GetAwaiter().GetResult()} ";
+                }
+
+                return result;
+            });
         }
     }
 }
