440b1b2ae6e5f3665e571243510ef8793f5d2884
Oleh Potomakha <potomakha@gmail.com>
2021-12-20 20:17:15 +0200

    Add project files.

---
+++ Module3HW5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Module3HW5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
